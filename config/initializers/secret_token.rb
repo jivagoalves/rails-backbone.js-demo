f06ff0e94edc3a8c3f7d5afe30c24e6681f3ebae
Jivago Alves <jivagoalves@gmail.com>
2012-02-04 19:31:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBackboneJsDemo::Application.config.secret_token = '68e8fdf53c63d3af22a56bd5262778fa032341073869f5f69363b73cd832253a08e9be18718e0a0c7c81564644548104da609af75eaa90327224223d3fdba16d'
